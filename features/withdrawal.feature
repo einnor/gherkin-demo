183fcae43e9a79efd780332d8078bbed309c6118
Ronnie Nyaga <ronnienyaga@microsoft.com>
2021-03-05 10:54:59 +0300

    Pass test to check for exceeding withdrawal limit

--- features/withdrawal.feature
+++ features/withdrawal.feature
@@ -10,3 +10,8 @@ Feature: Withdrawal
     Given I have Ksh. 1000 in my account
     When I withdraw Ksh. 1800
     Then My balance is Ksh. 1000
+
+    Scenario: Cash withdrawn is above withdrawal limit
+    Given I have Ksh. 100000 in my account
+    When I withdraw Ksh. 50000
+    Then My balance is Ksh. 100000
--- lib/BankAccount.ts
+++ lib/BankAccount.ts
@@ -1,4 +1,6 @@
 class BankAccount {
+  static WITHDRAWAL_LIMIT: number = 40000;
+
   balance: number = 0;
 
   setBalance (amount: number) {
@@ -10,9 +12,17 @@ class BankAccount {
   }
 
   withdraw (amount: number) {
-    if (this.balance > amount) {
+    if (!this._isOverdrawn(amount) && !this._hasExceededWithdrawalLimit(amount)) {
       this.balance = this.balance - amount;
     }
+  }
+
+  private _isOverdrawn(amount: number) {
+    return amount > this.balance;
+  }
+
+  private _hasExceededWithdrawalLimit (amount: number) {
+    return amount > BankAccount.WITHDRAWAL_LIMIT;
   }
 };
 
