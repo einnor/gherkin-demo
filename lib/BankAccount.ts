e098f4830aaa3d6643763aadc8a6e7cedbe51a9c
Ronnie Nyaga <ronnienyaga@microsoft.com>
2021-03-05 11:00:25 +0300

    Added code to pass  scenario - Cash withdrawn is below withdrawal limit

--- lib/BankAccount.ts
+++ lib/BankAccount.ts
@@ -1,5 +1,6 @@
 class BankAccount {
-  static WITHDRAWAL_LIMIT: number = 40000;
+  static MAXIMUM_WITHDRAWAL_LIMIT: number = 40000;
+  static MINIMUM_WITHDRAWAL_LIMIT: number = 40000;
 
   balance: number = 0;
 
@@ -12,7 +13,7 @@ class BankAccount {
   }
 
   withdraw (amount: number) {
-    if (!this._isOverdrawn(amount) && !this._hasExceededWithdrawalLimit(amount)) {
+    if (!this._isOverdrawn(amount) && !this._isAboveMaximumWithdrawalLimit(amount) && !this._isBelowMinimumWithdrawalLimit(amount)) {
       this.balance = this.balance - amount;
     }
   }
@@ -21,8 +22,12 @@ class BankAccount {
     return amount > this.balance;
   }
 
-  private _hasExceededWithdrawalLimit (amount: number) {
-    return amount > BankAccount.WITHDRAWAL_LIMIT;
+  private _isAboveMaximumWithdrawalLimit (amount: number) {
+    return amount > BankAccount.MAXIMUM_WITHDRAWAL_LIMIT;
+  }
+
+  private _isBelowMinimumWithdrawalLimit (amount: number) {
+    return amount < BankAccount.MINIMUM_WITHDRAWAL_LIMIT;
   }
 };
 
